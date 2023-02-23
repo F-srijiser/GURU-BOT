1aed6d0232281205cd9ecac18cb1fb3df84d2cd9
F-srijiser <126143194+F-srijiser@users.noreply.github.com>
2023-02-23 14:52:07 +0530

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -222,8 +222,8 @@ let m2 = `
 Bot has more than 300 commands and i am lazy to write all
 `
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
-    let pp = './Guru.jpg' 
-    conn.sendButton(m.chat, m2, '▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n', pp, [
+    let pp = './Screenshot_20220924_140019.jpg' 
+    conn.sendButton(m.chat, m2, '▢ SrijiSer ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/sriji_ser\n', pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
       ['⌬ Groups', `${usedPrefix}gpguru`]
     ],m, rpyt)
